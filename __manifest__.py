fd03804645ecb198cfdd14d4f82469a8c4cf1f60
Alexander Paiz <wpaiz@grupotecun.com>
2025-03-31 16:10:33 -0600

    Primer commit

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Asociacion de Vecinos',
+    'summary':"""
+        Implementacion Asociacion de Vecinos, Odoo 17
+    """,
+    'author':'Alexander Paiz',
+    'category': 'General',
+    'version' : '1.0.0',
+    'depends': [
+    ],
+    'data': [
+
+    ]
+}
